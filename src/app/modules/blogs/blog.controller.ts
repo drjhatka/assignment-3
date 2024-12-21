62d090a713c10e8f0630c733cfb68f5046afd937
drjhatka <bishawjit12@gmail.com>
2024-12-21 00:31:19 -0500

    Delete Blog route implemented

--- src/app/modules/blogs/blog.controller.ts
+++ src/app/modules/blogs/blog.controller.ts
@@ -10,54 +10,69 @@ import { User } from "../users/user.model";
 import { TBlog } from "./blog.interface";
 import { TUser } from "../users/user.interface";
 import { ObjectId } from 'mongodb';
+import Blog from "./blog.model";
 // @ts-ignore: Object is possibly 'null'.
 
-const getAllBlogs =catchAsync(async(req, res, next)=>{
-    res.send({message:'test'})
+const getAllBlogs = catchAsync(async (req, res, next) => {
+    res.send({ message: 'test' })
 
 })
-const getSingleBlog =catchAsync(async(req, res, next)=>{
+const getSingleBlog = catchAsync(async (req, res, next) => {
 
 })
-const createBlog=catchAsync(async(req, res, next)=>{
+const createBlog = catchAsync(async (req, res, next) => {
     //retrieve user email and role from jwt header as an array...
-        const decoded = retrieveUserCredentialsFromToken(req.headers.authorization as string, config.jwt_secret as string)
+    const decoded = retrieveUserCredentialsFromToken(req.headers.authorization as string, config.jwt_secret as string)
     //find user in the database with the email provided....
-        const user = await User.findOne({email:decoded.email}) 
-        //console.log(user)
+    const user = await User.findOne({ email: decoded.email })
+    //console.log(user)
     //assign user ID to blog author
-        const blog:TBlog={
-            title:req.body.title,
-            author: user._id,
-            content:req.body.content,
-            isPublished:true
-        };
+    const blog: TBlog = {
+        title: req.body.title,
+        author: user._id,
+        content: req.body.content,
+        isPublished: true
+    };
     const result = await BlogService.createBlogIntoDB(blog)
-    const data = await sanitizePostBlogData(result._id.toString(),['-isPublished'])
-    sendResponse(res, {success:true,  message:'Blog Created Successfully',statusCode:httpStatus.OK, data:data})
+    const data = await sanitizePostBlogData(result._id.toString(), ['-isPublished'])
+    sendResponse(res, { success: true, message: 'Blog Created Successfully', statusCode: httpStatus.OK, data: data })
 })
 
-const updateBlog=catchAsync(async(req, res, next)=>{ //ONLY User can update blog NOT Admin...
+const updateBlog = catchAsync(async (req, res, next) => { //ONLY User can update blog NOT Admin...
     //retrieve user email and role from jwt header as an array
     const decoded = retrieveUserCredentialsFromToken(req.headers.authorization as string, config.jwt_secret as string)
-    if(decoded.role ==='admin'){
-        sendResponse(res, {success:false, message:"Admin Cannot Update Blogs", statusCode:httpStatus.UNAUTHORIZED, data:{} })
+    if (decoded.role === 'admin') {
+        sendResponse(res, { success: false, message: "Admin Cannot Update Blogs", statusCode: httpStatus.UNAUTHORIZED, data: {} })
         return
     }
     // else{
-        const result=    await BlogService.updateBlog(req.params.id,req.body)
-        const updatedDoc = await sanitizePostBlogData(req.params.id,['_id','title','content','author'])
-        sendResponse(res, {success:false, message:"Blog updated Successfully!", statusCode:httpStatus.OK, data:updatedDoc })
+    const result = await BlogService.updateBlog(req.params.id, req.body)
+    const updatedDoc = await sanitizePostBlogData(req.params.id, ['_id', 'title', 'content', 'author'])
+    sendResponse(res, { success: false, message: "Blog updated Successfully!", statusCode: httpStatus.OK, data: updatedDoc })
     // }
 })
 
-const deleteBlog = catchAsync(async(req, res, next)=>{
+const deleteBlog = catchAsync(async (req, res, next) => {
     //admin can delete any blog but user can delete only their blog...
     const decoded = retrieveUserCredentialsFromToken(req.headers.authorization as string, config.jwt_secret as string)
-    if(decoded.role ==='user'){
+    //find blog and match user
+    const blogID = req.params.id;
+    const blog = await Blog.findById(blogID)
+    const user = await User.findById(blog.author)
+
+    if (decoded.role === 'user') {
         //check if the blog is authored by the current logged in user...
-        const blogID = req.params.id;
-        sendResponse(res, {success:false,statusCode:httpStatus.UNAUTHORIZED, message:"Users are not allowed to delete Blogs", data:{blogID} })
+        if (blog.author._id.toString() === user._id.toString()) {
+            const result = await BlogService.deleteBlog(blogID)
+            sendResponse(res, { success: true, statusCode: httpStatus.OK, message: "Blog Deleted", data: result })
+            return
+        }
+        sendResponse(res, { success: false, statusCode: httpStatus.UNAUTHORIZED, message: "User cannot delete other User's blog", data: { blogID } })
+        return
+    }
+    else {
+        const result = await BlogService.deleteBlog(blogID)
+        sendResponse(res, { success: true, statusCode: httpStatus.OK, message: "Blog Deleted", data: result })
         return
     }
 })
--- src/app/modules/blogs/blog.service.ts
+++ src/app/modules/blogs/blog.service.ts
@@ -2,17 +2,21 @@ import { TBlog } from "./blog.interface";
 import Blog from "./blog.model";
 import { ObjectId } from 'mongodb';
 
-const createBlogIntoDB =async (payload:TBlog)=>{
+const createBlogIntoDB = async (payload: TBlog) => {
     //assign current logged in user id to blog author field id...
     return (await Blog.create(payload)).populate('author')
 }
 
-const updateBlog = async(id:string, payload:Partial<TBlog>)=>{
-    console.log(id, payload)
-    return await Blog.findOneAndUpdate({_id:new ObjectId(id)}, payload).select(['_id','title','content','author']).populate('author')
+const updateBlog = async (id: string, payload: Partial<TBlog>) => {
+    return await Blog.findOneAndUpdate({ _id: new ObjectId(id) }, payload).select(['_id', 'title', 'content', 'author']).populate('author')
+}
+
+const deleteBlog = async (id: string)=>{
+    return await Blog.deleteOne({ _id: new ObjectId(id) })
 }
 
 export const BlogService = {
     createBlogIntoDB,
-    updateBlog
+    updateBlog,
+    deleteBlog
 }
